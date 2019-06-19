2468db537f46dd50201af6b1f2ee1d518816c50a
Mino Mutekihito <nguyenkhoa147369@gmail.com>
2019-06-19 11:01:12 +0700

    front-end, back-end, chua co xu ly db

--- Web/source-code/HiddenusWeb/src/main/java/com/hiddenus/controller/ProductController.java
+++ Web/source-code/HiddenusWeb/src/main/java/com/hiddenus/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.hiddenus.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -10,8 +12,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hiddenus.model.Product;
+import com.hiddenus.model.User;
 import com.hiddenus.service.ProductService;
 
 @Controller
@@ -26,10 +30,21 @@ public class ProductController {
 		Page<Product> product = productService.showProduct(currentPage, pageSize);
         return product;
 	}*/
-	@RequestMapping("/products")
-   // @GetMapping(value = { "", "/" })
-    public @NotNull Iterable<Product> getProducts() {
+	//Hien thi tat ca product theo Page
+	
+	
+   /* public @NotNull Iterable<Product> getProducts() {
         return productService.getAllProducts();
-    }
         
+    }*/
+	//GetAllProduct
+	@RequestMapping(value = "/product/MenShortSleeve", method = RequestMethod.GET)
+    @GetMapping(value = { "", "/" })
+	public ModelAndView product() {
+		  ModelAndView model = new ModelAndView();
+		  model.setViewName("product/MenShortSleeves");
+		  return model;
+		  
+		 }
+	//chua hieu cai nay 
 }
